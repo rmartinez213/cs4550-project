4461042bd10636ec813086c6755387fc194cc9e7
rmartinez213 <rmartinez213@yahoo.com>
2018-11-08 19:40:34 -0800

    first push of working nav bar

---
+++ app/src/main/java/com/example/rmart1nez/fortniteapp/MainActivity.java
@@ -0,0 +1,98 @@
+package com.example.rmart1nez.fortniteapp;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout drawer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        /* Creates Menu button on the top left corner  */
+        drawer = findViewById(R.id.drawer_layout);
+
+        /* Set the navigation view for the view */
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+
+        drawer.addDrawerListener(toggle);
+
+        /* Handles the icon */
+        toggle.syncState();
+
+
+        /* if we see our activity for the first time  */
+        if(savedInstanceState == null){
+
+        /* Display main screen window when application loads */
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new PlayerStats()).commit();
+        navigationView.setCheckedItem(R.id.nav_player_stats);
+
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+        /* Finds out which item was clicked */
+        switch (menuItem.getItemId()) {
+            case R.id.nav_player_stats:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new PlayerStats()).commit();
+                break;
+
+            case R.id.nav_match_history:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new MatchHistory()).commit();
+                break;
+
+            case R.id.nav_item_shop:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ItemShop()).commit();
+                break;
+
+            case R.id.nav_challenges:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new Challenges()).commit();
+                break;
+
+            case R.id.nav_news:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new News()).commit();
+                break;
+
+            case R.id.nav_server_status:
+                Toast.makeText(this, "INSERT SERVER STATUS HERE", Toast.LENGTH_SHORT).show();
+        }
+
+        /* To close the navigation window (drawer) */
+        drawer.closeDrawer(GravityCompat.START);
+
+        return true;
+    }
+
+    @Override
+    public void onBackPressed(){
+        if (drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        }
+        else{
+            super.onBackPressed();
+        }
+    }
+}
